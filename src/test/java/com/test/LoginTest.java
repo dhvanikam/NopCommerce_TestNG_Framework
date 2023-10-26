4072905a3c032116cf978b95fa18b3424aed12e6
Pavithra-kamat <pavithra.s15@gmail.com>
2023-10-26 16:51:29 -0700

    First commit to branch -pavithra

---
+++ src/test/java/com/test/LoginTest.java
@@ -0,0 +1,26 @@
+package com.test;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import com.baseClass.BaseClass;
+
+public class LoginTest extends BaseClass{
+	@Test(dataProvider="getData",groups= {"Login"})
+	public void loginTest(HashMap<String,String> input)
+	{
+		loginPage.loginApplication(input.get("email"), input.get("password"));
+		
+	}
+	@DataProvider
+	public Object[][] getData() throws IOException
+	{
+		List<HashMap<String,String>> data = getJsonDataToMap(System.getProperty("user.dir")+"//src//test//resources//Data//LoginCredentials.json");
+		return new Object[][]  {{data.get(0)}, {data.get(1) } };
+		
+	}
+}
