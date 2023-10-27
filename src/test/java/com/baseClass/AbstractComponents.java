f2ab0f61564dfd11ea2c00690806903d6a4603d2
Pavithra-kamat <pavithra.s15@gmail.com>
2023-10-27 08:32:59 -0700

    Login page and Test added -pavithra

--- src/test/java/com/baseClass/AbstractComponents.java
+++ src/test/java/com/baseClass/AbstractComponents.java
@@ -6,8 +6,7 @@ import org.openqa.selenium.support.PageFactory;
 public class AbstractComponents {
 	WebDriver driver;
 
-	public AbstractComponents(WebDriver driver) {
-		
+	public AbstractComponents(WebDriver driver) {	
 		this.driver = driver;
 		PageFactory.initElements(driver, this);	
 	}
--- src/test/java/com/test/LoginTest.java
+++ src/test/java/com/test/LoginTest.java
@@ -4,23 +4,40 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
 
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import com.baseClass.BaseClass;
+import com.pages.LoginPage;
+import com.pages.Page;
+import com.utility.Log;
 
-public class LoginTest extends BaseClass{
-	@Test(dataProvider="getData",groups= {"Login"})
-	public void loginTest(HashMap<String,String> input)
-	{
-		//loginPage.loginApplication(input.get("email"), input.get("password"));
-		
+public class LoginTest extends BaseClass {
+	LoginPage loginPage;
+
+	@BeforeMethod
+	public void setup() {
+		loginPage = new LoginPage();
 	}
-	//@DataProvider
-	//public Object[][] getData() throws IOException
-	//{
-		//List<HashMap<String,String>> data = getJsonDataToMap(System.getProperty("user.dir")+"//src//test//resources//Data//LoginCredentials.json");
-		//return new Object[][]  {{data.get(0)}, {data.get(1) } };
-		
-	//}
+
+	@Test(dataProvider = "getData", groups = { "Login" })
+	public void loginTest(HashMap<String, String> input) {
+
+		loginPage.loginApplication(input.get("email"), input.get("password"));
+		String errorMsg = loginPage.getErrorMessage();
+		if (errorMsg == "")
+			Log.logInfo("Success full Login");
+		else
+			Log.logInfo(errorMsg);
+
+	}
+
+	@DataProvider
+	public Object[][] getData() throws IOException {
+		List<HashMap<String, String>> data = getJsonDataToMap(
+				System.getProperty("user.dir") + "//src//test//resources//Data//LoginCredentials.json");
+		return new Object[][] { { data.get(0) }, { data.get(1) } };
+
+	}
 }
